e4e771ed2fecc9dada198cdccc77115b70ca8cd8
Erick Vidbaz <7471536+vidbaz@users.noreply.github.com>
2022-02-13 18:21:02 -0600

    added EC2 instance configuration and AMI information related

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ variable "subnet_cidr_blocks" {}
 variable "availability_zone" {}
 variable "env_postfix" {}
 variable "personal_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -72,8 +74,7 @@ resource "aws_default_route_table" "rtb-main" {
 
 #Open ports 22 and 8080 with a security group
 
-resource "aws_security_group" "sg-1" {
-  name = "sg_1"
+resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.vpc.id
   ingress = [{
       from_port = 22
@@ -115,4 +116,53 @@ resource "aws_security_group" "sg-1" {
     Name = "sg-1_${var.env_postfix}"
     Env = var.env_postfix
   }
+}
+
+# Get information from AWS
+data "aws_ami" "linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+      name = "name"
+      values = ["amzn2-ami-kernel-5*"]
+  }
+}
+
+output "aws_ami_linux" {
+    value = data.aws_ami.linux-image.id
+}
+
+# Upload your ssh public key 
+resource "aws_key_pair" "dev-key" {
+  key_name   = "dev-key"
+  public_key = file(var.public_key_location)
+}
+
+# Create EC2 Instance
+# ssh ec2-user@3.144.21.117
+resource "aws_instance" "ec2-server-1" {
+  ami = data.aws_ami.linux-image.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.availability_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.dev-key.key_name
+
+  user_data = <<EOF
+  #!/bin/bash
+  sudo yum update -y && sudo yum install -y docker
+  sudo systemctl start docker
+  sudo usermod -aG docker ec3-user
+  docker run -p 8080:80 nginx
+  EOF
+
+  tags = {
+    Name = "webserver_${var.env_postfix}"
+    Env = var.env_postfix
+  }
+}
+
+output "ec2-server-1-public-ip" {
+    value = aws_instance.ec2-server-1.public_ip
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,5 @@ vpc_cidr_blocks = "10.0.0.0/16"
 subnet_cidr_blocks = "10.0.10.0/24"
 availability_zone = "us-east-2a"
 env_postfix = "dev"
+instance_type = "t2.micro" # Free Tier 
+public_key_location = "~/.ssh/id_rsa.pub"
